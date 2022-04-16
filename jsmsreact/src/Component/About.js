94191aff9fe815b43b178ae845da0bb12ae85f40
ParagMadhwai <98086301+ParagMadhwai@users.noreply.github.com>
2022-04-16 08:20:19 +0530

    Add files via upload

---
+++ jsmsreact/src/Component/About.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="accordion accordion-flush" id="accordionFlushExample">
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingOne">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        What is JSMS?
+                    </button>
+                </h2>
+                <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">When it comes to functionality, features, and service, the Jewellery shop management system(JSMS) is considered the market leader. The software is specially developed to simplify and streamline all facets of the jewellery business, thereby benefiting all parties involved in the process – the manufacturer, the retailer, and the customers.
+                        Our jewellery management system is simple to use and quick to implement, besides saving a considerable operational cost in the long run. The system will ensure that every process, from purchasing to manufacture to sales, is carried out in a structured and error-free manner. Ultimately, this will increase both organizational productivity and customer satisfaction.
+                        Additionally, you can also carry out a number of peripheral tasks associated with your business using JSMS, such as Accounting, Inventory, Employee Management, Bills and so on. In a nutshell, JSMS software is more than just a sales tool.</div>
+                </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingTwo">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        Accordion Item #2
+                    </button>
+                </h2>
+                <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingThree">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                        Accordion Item #3
+                    </button>
+                </h2>
+                <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                </div>
+            </div>
+        </div>
+    )
+}
